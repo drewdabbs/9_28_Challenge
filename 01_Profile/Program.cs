88e6696be447b376007c1d61389278274170bb40
Drew Dabbs <dabbs.drew@gmail.com>
2021-09-29 00:08:27 -0400

    Cannot get age to work

--- 01_Profile/Program.cs
+++ 01_Profile/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _01_ProfileClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,43 @@ namespace _01_Profile
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            //Bronze
+            FacebookProfile user = new FacebookProfile();
+            user.FirstName = "Richard";
+            user.LastName = "Lionheart";
+            user.AgeInInt = 44;
+            user.Occupation = "King of Messenger";
+            user.YearsAtJob = 12;
+            Console.WriteLine(user.Occupation);
+            Console.ReadLine();
+
+            //Silver
+            FacebookProfile fullName = new FacebookProfile();
+            string full = fullName.FullName;
+            Console.WriteLine(full);
+            Console.ReadLine();
+            
+
+            //Gold
+
+            FacebookProfile newUser = new FacebookProfile("Paul", "Revere", "male", "Silversmith", 22);
+            Console.WriteLine(newUser.FullName, "\n");
+            Console.WriteLine(newUser.Gender, "\n");
+            Console.WriteLine(newUser.Occupation, "\n");
+            Console.WriteLine(newUser.YearsAtJob);
+            Console.ReadLine();
+
+            
+            
+        {
+                {
+                    
+                } 
+                
         }
+    }
     }
 }
---
+++ ClassLibrary1/01_ProfileClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ProfileClass
+{
+    public class FacebookProfile
+    {
+        //Bronze Facebook Profile Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName  // Part of Silver
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        public int AgeInYears
+
+        {
+            get
+            {
+                TimeSpan yearSpan = DateTime.Now - DateOfBirth;
+                double ageInYears = yearSpan.Days / 365.25;
+                return Convert.ToInt32(Math.Floor(ageInYears));
+
+             }
+        }
+        public int AgeInInt { get; set; }
+        public string Occupation { get; set; }
+        public int YearsAtJob { get; set; }
+        public string Gender { get; set; }
+
+
+        public DateTime DateOfBirth { get; set; }
+
+
+        public FacebookProfile()
+        {
+
+        }
+
+        public FacebookProfile(string firstName, string lastName, string gender, string occupation, int yearsAtJob)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Occupation = occupation;
+            YearsAtJob = yearsAtJob;
+
+
+        }
+
+
+
+    }
+}
